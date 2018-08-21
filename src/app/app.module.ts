3e4eb47acde8cf37f0ad40c05aa3c3456bd4b3fb
Hitesh C <jainhitesh9998@gmail.com>
2018-08-22 00:26:43 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {WebcamModule} from 'ngx-webcam';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
